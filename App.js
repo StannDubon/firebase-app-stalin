d0e782bb6f9d9e4c003b56f379805af3767cf5ca
StannDubon <stalin.dbn@gmail.com>
2024-07-04 15:43:26 -0600

    cambios uwu

--- App.js
+++ App.js
@@ -18,3 +18,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// API_KEY = AIzaSyCfzo3iGr2YRtoH2r7n-l5UQ2BuAB3UFfg
+// AUTH_DOMAIN = practica-firebase-20220075.firebaseapp.com
+// PROJECT_ID = practica-firebase-20220075
+// STORAGE_BUCKET = practica-firebase-20220075.appspot.com
+// MESSAGING_SENDER_ID = 930261289191
+// APP_ID = 1:930261289191:web:c2e59bdeaca18d4b573a42
